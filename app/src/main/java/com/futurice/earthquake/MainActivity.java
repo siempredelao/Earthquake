a972191a2354d03a20ddab943a60606081c62f64
David Aguiar <siempredelao@gmail.com>
2017-03-01 14:46:14 +0100

    Adding ButterKnife and RecyclerView libraries and injecting views in MainActivity

--- app/src/main/java/com/futurice/earthquake/MainActivity.java
+++ app/src/main/java/com/futurice/earthquake/MainActivity.java
@@ -1,13 +1,24 @@
 package com.futurice.earthquake;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_activity_recyclerview)
+    protected RecyclerView recyclerview;
+    @BindView(R.id.main_activity_progressbar)
+    protected ProgressBar  progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
